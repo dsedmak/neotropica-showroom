761adf54414b820de0446efc23e5fbb620b1fe3a
Oksana Ivanovič <oksana.ivanovic98@gmail.com>
2025-06-12 10:32:21 +0200

    Main commit 01

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ export default defineConfig({
         theme_color: '#13171f',
         background_color: '#13171f',
         display: 'standalone',
-        start_url: '/neotropica-showroom/sl/hallway/'
+        start_url: '/neotropica-showroom/sl/main/'
       },
 
       workbox: {
